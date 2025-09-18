64ed54e65d909f7f9da252d7591931fc906bfb53
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-09-18 18:29:33 +0500

    ¯\_(ツ)_/¯

--- rofi/wallselect/script.sh
+++ rofi/wallselect/script.sh
@@ -15,13 +15,15 @@ SELECTED=$(find "$WALL_DIR" \
     -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" -o -iname "*.gif" \) \
     | shuf \
     | while read -r img; do
-        echo -en "$img\0icon\x1f$img\n"
+        fname=$(basename "$img")   # extract just filename
+        echo -en "$fname\0icon\x1f$img\n"
     done \
     | rofi -dmenu -show-icons -theme "$HOME/.config/rofi/wallselect/style.rasi")
 
-# If user selected a wallpaper, set it with swww
+# If user selected a wallpaper, find full path and set it with swww
 if [ -n "$SELECTED" ]; then
-    swww img "$SELECTED" \
+    FULLPATH=$(find "$WALL_DIR" -type f -iname "$SELECTED" | head -n 1)
+    swww img "$FULLPATH" \
         --transition-type grow \
         --transition-duration 0.5 \
         --transition-fps 144
