4caf646d5383f76e205663a4a312f1a60d461575
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-03 22:32:29 +0500

    todo: Made it so that swtiching modes is instant, no delay

--- nvim/lua/custom/options.lua
+++ nvim/lua/custom/options.lua
@@ -3,7 +3,9 @@
 local opt = vim.opt
 
 opt.updatetime = 200         -- Faster diagnostics / cursor responsiveness
-opt.timeoutlen = 300         -- Shorter keymap timeout
+opt.timeout = false          -- Disable timeout for mappings
+opt.ttimeoutlen = 0          -- No delay for key codes
+opt.timeoutlen = 100         -- Shorter keymap timeout (for when timeout is true)
 opt.lazyredraw = true        -- Redraw only when needed
 opt.swapfile = false         -- No swap files
 opt.backup = false           -- No backups
