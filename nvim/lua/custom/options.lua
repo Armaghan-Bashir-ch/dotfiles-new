97ea2fdb1421c0be4e40ccfe47c2c4c970a10063
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-04 16:33:35 +0500

    feat: Fixed some problems with escape key not rendering in vim

--- nvim/lua/custom/options.lua
+++ nvim/lua/custom/options.lua
@@ -2,11 +2,10 @@
 
 local opt = vim.opt
 
-opt.updatetime = 200         -- Faster diagnostics / cursor responsiveness
-opt.timeout = false          -- Disable timeout for mappings
-opt.ttimeoutlen = 0          -- No delay for key codes
-opt.timeoutlen = 100         -- Shorter keymap timeout (for when timeout is true)
 opt.lazyredraw = true        -- Redraw only when needed
 opt.swapfile = false         -- No swap files
 opt.backup = false           -- No backups
 opt.cursorlineopt = "number" -- Highlight only number in cursorline
+opt.timeout = false
+opt.ttimeout = false
+opt.ttimeoutlen = 0
