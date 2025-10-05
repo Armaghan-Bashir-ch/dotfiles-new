417b236bfd566cafeab22ef6d0813a4ea1a5454e
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-05 17:22:07 +0500

    Feat: Added new mappings

--- nvim/lua/custom/configs/plenary.lua
+++ nvim/lua/custom/configs/plenary.lua
@@ -11,10 +11,19 @@ local config = function()
     return vim.fn.filereadable(wakatime_cli_path) == 1
   end
 
+  -- Store the last notification time
+  local last_notification_time = 0
+
   -- Define an asynchronous function to get today's WakaTime usage
   local get_wakatime_time = async.void(function()
     if not is_wakatime_cli_available() then
       vim.notify("WakaTime CLI not found at " .. wakatime_cli_path, vim.log.levels.WARN)
+      return
+    end
+
+    -- Check if 10 minutes (600 seconds) have passed since the last notification
+    local current_time = os.time()
+    if current_time - last_notification_time < 600 then
       return
     end
 
@@ -34,7 +43,10 @@ local config = function()
     job:start()
     local result = rx()
     if result then
-      vim.schedule(function() vim.notify(result, vim.log.levels.INFO) end)
+      vim.schedule(function()
+        vim.notify(result, vim.log.levels.INFO)
+        last_notification_time = os.time()
+      end)
     end
   end)
 
