e710357aad6ca081cb602b57dedebdce1a30f1b3
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-11 20:08:01 +0500

    chore: changed themes, and notify-send operations

--- hypr/scripts/theme-switcher.sh
+++ hypr/scripts/theme-switcher.sh
@@ -78,7 +78,7 @@ fi
 # Reload waybar
 pkill -SIGUSR2 waybar
 
-notify-send -i "$WALLPAPER_PATH" "Theme Switcher" "Switched to $THEME theme"
+notify-send -h string:image-path:"$WALLPAPER_PATH" "Theme Switcher" "Switched to $THEME theme"
 
 # Save current theme for other scripts
 echo "$THEME" > ~/.config/hypr/current_theme.txt
