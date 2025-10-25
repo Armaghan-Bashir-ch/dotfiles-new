607924190c50546996ade2f0d1bc113fc34b6aef
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-25 14:20:14 +0500

    feat: set lazy=false, so that it runs every time ons tartup. the todo-comments plugin

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -269,6 +269,7 @@ local plugins = {
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = false,
         opts = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
@@ -998,7 +999,7 @@ local plugins = {
     {
         "letieu/btw.nvim",
         config = function()
-            require('btw').setup({
+            require("btw").setup({
                 text = "I use Neovim with Arch (BTW)",
             })
         end,
