526468197736b15e55046ab064c4cb5493804cf1
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-25 14:34:25 +0500

    fix: Options for todo-comments

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -271,9 +271,7 @@ local plugins = {
         dependencies = { "nvim-lua/plenary.nvim" },
         lazy = false,
         opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
+            -- config go here
         },
     },
 
