a82e1b595b485477387bcfd740a1e86d79885b7e
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-10-25 20:06:15 +0500

    fix: f/t motions

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -172,8 +172,6 @@ M.code_refactor = {
     },
 }
 
-
-
 M.rulebook = {
     plugin = true,
     n = {
@@ -647,17 +645,6 @@ M.general = {
         ["<leader>b9"] = { "<cmd>buffer 9<CR>", "Select buffer 9" },
         ["<leader>oo"] = { "<cmd>vsplit | term opencode<CR>", "Open opencode" },
         ["<leader>cc"] = { "<cmd>vsplit | term crush<CR>", "Open Crush" },
-        ["f"] = { "f", "f motion forwards" },
-        ["t"] = { "F", "f motion backwards" },
-        ["F"] = { "F", "F motion forwards" },
-        ["T"] = { "T", "T motion backwards" },
-    },
-
-    o = {
-        ["f"] = { "f", "f motion forwards" },
-        ["t"] = { "F", "f motion backwards" },
-        ["F"] = { "F", "F motion forwards" },
-        ["T"] = { "T", "T motion backwards" },
     },
 
     v = {
@@ -708,10 +695,6 @@ M.general = {
             ":m '<-2<CR>gv=gv",
             "Move selected lines up",
         },
-        ["f"] = { "f", "f motion forwards" },
-        ["t"] = { "F", "f motion backwards" },
-        ["F"] = { "F", "F motion forwards" },
-        ["T"] = { "T", "T motion backwards" },
     },
 
     x = {
@@ -732,10 +715,6 @@ M.general = {
             [["_dP"]],
             "Paste without overwriting register",
         },
-        ["f"] = { "f", "f motion forwards" },
-        ["t"] = { "F", "f motion backwards" },
-        ["F"] = { "F", "F motion forwards" },
-        ["T"] = { "T", "T motion backwards" },
     },
 
     i = {
