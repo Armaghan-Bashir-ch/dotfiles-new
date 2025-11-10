790a184a5e2749ccdf86e3dea37f3eb7d2817b4f
Armaghan Bashir <armaghanbashirch3@gmail.com>
2025-11-10 22:36:16 +0500

    feat: Changed mapping/added right desc

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -282,11 +282,8 @@ vim.diagnostic.config({
 })
 
 vim.api.nvim_create_user_command("Timer", function()
-  vim.o.showtabline = 0
-  vim.o.laststatus = 0
   vim.wo.number = false
   vim.o.scl = "no"
-  vim.o.cmdheight = 0
   require("timers.ui").dashboard()
 end, {})
 
